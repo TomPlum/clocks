5901b31fdd0cd56607168e07d54b130d486d49c3
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-07-28 19:00:10 +0100

    feat(clocks): fixed linting error (again - is it broken??)

--- src/context/ThemeContext/useThemeContext.ts
+++ src/context/ThemeContext/useThemeContext.ts
@@ -1,4 +1,4 @@
 import { useContext } from 'react'
-import { ThemeContext } from './ThemeContext.ts'
+import { ThemeContext } from './ThemeContext'
 
 export const useThemeContext = () => useContext(ThemeContext)
