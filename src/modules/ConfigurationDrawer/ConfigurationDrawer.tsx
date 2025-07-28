d0c32fb19137b7445bab92fa88048193c5d3069d
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-07-28 18:40:04 +0100

    feat(clocks): added manual skip time buttons

--- src/modules/ConfigurationDrawer/ConfigurationDrawer.tsx
+++ src/modules/ConfigurationDrawer/ConfigurationDrawer.tsx
@@ -4,11 +4,12 @@ import type { ConfigurationDrawerProps } from 'modules/ConfigurationDrawer/types
 import { ManualTimeSelector } from 'modules/ConfigurationDrawer/components/ManualTimeSelector'
 import styles from './ConfigurationDrawer.module.scss'
 import { ResetTimeButton } from 'modules/ConfigurationDrawer/components/ResetTimeButton'
+import { SkipTimeButtons } from './components/SkipTimeButtons'
 
 export const ConfigurationDrawer = ({ opened, onClose }: ConfigurationDrawerProps) => {
   return (
     <Drawer
-      size='sm'
+      size='xs'
       opened={opened}
       onClose={onClose}
       position='right'
@@ -17,9 +18,18 @@ export const ConfigurationDrawer = ({ opened, onClose }: ConfigurationDrawerProp
         body: styles.Drawer__Content
       }}
     >
-      <ThemeSelector />
-      <ManualTimeSelector />
-      <ResetTimeButton />
+      <div className={styles.Section}>
+        <ThemeSelector />
+      </div>
+
+      <div className={styles.Section}>
+        <ManualTimeSelector />
+        <SkipTimeButtons />
+      </div>
+
+      <div className={styles.Section}>
+        <ResetTimeButton />
+      </div>
     </Drawer>
   )
 }
