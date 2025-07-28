43123796c2fa35096ae12dfc12402c5daa4b3043
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-07-28 19:24:58 +0100

    feat(clocks): added inspiration text to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { useThemeContext } from 'context/ThemeContext'
 import { SettingsButton } from 'modules/ConfigurationDrawer/components/SettingsButton'
 import { useDisclosure } from '@mantine/hooks'
 import { ConfigurationDrawer } from 'modules/ConfigurationDrawer'
-import { MantineProvider } from '@mantine/core'
+import { Anchor, MantineProvider, Typography } from '@mantine/core'
 import { forwardRef } from 'react'
 
 export const Layout = forwardRef<TimeDisplayRefHandle>((_, timeDisplayRef) => {
@@ -21,6 +21,21 @@ export const Layout = forwardRef<TimeDisplayRefHandle>((_, timeDisplayRef) => {
         className={styles.Container}
         style={{ backgroundColor: themeColours.backgroundColour }}
       >
+        <Typography className={styles.Container__Inspiration}>
+          Inspired by 'A million times' by
+
+          <Anchor
+            c='yellow'
+            target='_blank'
+            underline='never'
+            className={styles.Link}
+            href='https://www.humanssince1982.com/en-uk/pages/studio-story'
+          >
+            Humans Since 1982
+          </Anchor>
+          .
+        </Typography>
+
         <ConfigurationDrawer
           opened={opened}
           onClose={close}
